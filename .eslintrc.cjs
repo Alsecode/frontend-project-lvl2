9e298bfd24477d8429d80845efab426ee558b8d6
Art <fil.flip@mail.ru>
2022-08-13 19:02:04 +0300

    changed eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,23 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  "env": {
+    "browser": true,
+    "es2021": true,
   },
-  plugins: [
-    'jest',
+  "extends": [
+    "airbnb-base",
+	"plugin:jest/recommended"
   ],
-  extends: [
-    'airbnb-base',
+  "parser": '@typescript-eslint/parser',
+  "parserOptions": {
+    "ecmaVersion": 'latest',
+    "sourceType": 'module',
+  },
+  "plugins": [
+    '@typescript-eslint',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'no-console': 0,
-    'import/extensions': 0, 
-    'no-underscore-dangle': [2, { "allow": ["__filename", "__dirname"] }],
-  },
+  "rules": {
+  	  "no-console": 0,
+	  "import/extensions": 0,
+  	  "no-underscore-dangle": [2, { "allow": ["__filename", "__dirname"] }],
+   },
 };
